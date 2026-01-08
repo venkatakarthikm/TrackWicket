1d0ed7175355888410466e4381854294f233a696
vkarthikm <virat18mvk@gmail.com>
2026-01-08 15:12:56 +0530

    tw load

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,14 +1,18 @@
 // Loader.jsx
 import "./loader.css"
 const Loader = () => (
-  <div className="loader">
-    {Array.from({ length: 9 }).map((_, i) => (
-      <div className="text" key={i}>
-        <span>TRACKWICKET</span>
-      </div>
-    ))}
-    <div className="line"></div>
-  </div>
+  <div class="loader">
+  <div class="text"><span>TRACKWICKET</span></div>
+  <div class="text"><span>TRACKWICKET</span></div>
+  <div class="text"><span>TRACKWICKET</span></div>
+  <div class="text"><span>TRACKWICKET</span></div>
+  <div class="text"><span>TRACKWICKET</span></div>
+  <div class="text"><span>TRACKWICKET</span></div>
+  <div class="text"><span>TRACKWICKET</span></div>
+  <div class="text"><span>TRACKWICKET</span></div>
+  <div class="text"><span>TRACKWICKET</span></div>
+  <div class="line"></div>
+</div>
 );
 
 export default Loader;
